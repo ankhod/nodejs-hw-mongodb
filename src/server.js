b948b440e5b649ddd86bd43745ecd827b922d7c0
ankhod <ankhod7@gmail.com>
2025-05-10 17:12:02 +0300

    Step 1 server.js

---
+++ src/server.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import cors from 'cors';
+import pino from 'pino-http';
+
+export const setupServer = () => {
+  const app = express();
+
+  // Middleware
+  app.use(cors());
+  app.use(pino());
+  app.use(express.json());
+
+  // Обробка неіснуючих роутів
+  app.use((req, res) => {
+    res.status(404).json({ message: 'Not found' });
+  });
+
+  // Запуск сервера
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+
+  return app;
+};
