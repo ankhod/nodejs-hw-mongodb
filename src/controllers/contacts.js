eaadfc57ae24f153485067aef11e7a59fd40ac44
ankhod <ankhod7@gmail.com>
2025-05-10 21:02:08 +0300

    GET /contacts/:contactId

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -1,10 +1,16 @@
-import { getAllContacts } from '../services/contacts.js';
+import { getContactById } from '../services/contacts.js';
 
-export const getContactsController = async (req, res) => {
-  const contacts = await getAllContacts();
+export const getContactByIdController = async (req, res) => {
+  const { contactId } = req.params;
+  const contact = await getContactById(contactId);
+
+  if (!contact) {
+    return res.status(404).json({ message: 'Contact not found' });
+  }
+
   res.status(200).json({
     status: 200,
-    message: 'Successfully found contacts!',
-    data: contacts,
+    message: `Successfully found contact with id ${contactId}!`,
+    data: contact,
   });
 };
