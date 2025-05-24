2a3c5fdcd342e66e8005c05f1ecb5b64c7cb8c3f
ankhod <ankhod7@gmail.com>
2025-05-24 21:55:27 +0300

    renovated getAllContacts in services/contacts.js

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,7 +1,19 @@
 import { Contact } from '../models/contact.js';
 
-export const getAllContacts = async () => {
-  return await Contact.find();
+export const getAllContacts = async ({ page = 1, perPage = 10 }) => {
+  const skip = (page - 1) * perPage;
+  const totalItems = await Contact.countDocuments();
+  const contacts = await Contact.find().skip(skip).limit(perPage);
+
+  return {
+    data: contacts,
+    totalItems,
+    page: Number(page),
+    perPage: Number(perPage),
+    totalPages: Math.ceil(totalItems / perPage),
+    hasPreviousPage: page > 1,
+    hasNextPage: page < Math.ceil(totalItems / perPage),
+  };
 };
 
 export const getContactById = async (contactId) => {
@@ -13,7 +25,9 @@ export const createContact = async (contactData) => {
 };
 
 export const updateContact = async (contactId, updateData) => {
-  return await Contact.findByIdAndUpdate(contactId, updateData, { new: true });
+  return await Contact.findByIdAndUpdate(contactId, updateData, {
+    new: true,
+  });
 };
 
 export const deleteContact = async (contactId) => {
