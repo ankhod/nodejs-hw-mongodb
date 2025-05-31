f8ff09afde2e1600e62d1df8cfffb5a0fc26f719
ankhod <ankhod7@gmail.com>
2025-05-31 21:39:25 +0300

    update services/auth.js

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,6 +1,10 @@
 import { User } from '../models/user.js';
+import { Session } from '../models/session.js';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 import createHttpError from 'http-errors';
+
+const JWT_SECRET = process.env.JWT_SECRET;
 
 export const registerUser = async ({ name, email, password }) => {
   const existingUser = await User.findOne({ email });
@@ -18,4 +22,38 @@ export const registerUser = async ({ name, email, password }) => {
   // Видаляємо пароль із поверненого об’єкта
   user.password = undefined;
   return user;
+};
+
+export const loginUser = async ({ email, password }) => {
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw createHttpError(401, 'Unauthorized');
+  }
+
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+  if (!isPasswordValid) {
+    throw createHttpError(401, 'Unauthorized');
+  }
+
+  // Видаляємо стару сесію, якщо вона є
+  await Session.deleteOne({ userId: user._id });
+
+  // Створюємо токени
+  const accessToken = jwt.sign({ userId: user._id }, JWT_SECRET, {
+    expiresIn: '15m',
+  });
+  const refreshToken = jwt.sign({ userId: user._id }, JWT_SECRET, {
+    expiresIn: '30d',
+  });
+
+  // Створюємо нову сесію
+  const session = await Session.create({
+    userId: user._id,
+    accessToken,
+    refreshToken,
+    accessTokenValidUntil: new Date(Date.now() + 15 * 60 * 1000), // 15 хвилин
+    refreshTokenValidUntil: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000), // 30 днів
+  });
+
+  return { user, accessToken, refreshToken };
 };
